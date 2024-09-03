893e971d5c84b37867c8f12c8e6c1668947406ff
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-09-03 12:00:53 +0200

    AppFixtures ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,136 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Etat;
+use Faker\Generator;
+use App\Entity\Ville;
+use Faker\Factory;
+use App\Entity\Lieu;
+use App\Entity\Campus;
+use App\Entity\Participant;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Sortie;
 
 class AppFixtures extends Fixture
+
+
 {
+    private readonly Generator $faker;
+
+    public function __construct(private readonly UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->addEtats($manager);
+        $this->addVilles(10, $manager);
+        $this->addLieux(20, $manager);
+        $this->addCampus($manager);
+        $this->addParticipants(20, $manager);
+        $this->addSorties(20, $manager);
+    }
+
+    public function addEtats(ObjectManager $manager): void
+    {
+        $libelles = ['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée'];
+
+            foreach ($libelles as $libelle) {
+                $etat = new Etat();
+                $etat->setLibelle($libelle);
+                $manager->persist($etat);
+            }
+
+            $manager->flush();
+    }
+
+    public function addVilles(int $number, ObjectManager $manager): void
+    {
+        for ($i = 0; $i < $number; $i++) {
+            $ville = new Ville();
+            $ville->setNom($this->faker->city);
+            $ville->setCodePostal($this->faker->postcode);
+            $manager->persist($ville);
+        }
 
         $manager->flush();
     }
+
+    public function addLieux(int $number, ObjectManager $manager): void
+    {
+
+        $villes = $manager->getRepository(Ville::class)->findAll();
+        for ($i = 0; $i < $number; $i++) {
+            $lieu = new Lieu();
+            $lieu->setNom($this->faker->company);
+            $lieu->setRue($this->faker->streetAddress);
+            $lieu->setLatitude($this->faker->latitude);
+            $lieu->setLongitude($this->faker->longitude);
+            $lieu->setVille($this->faker->randomElement($villes));
+            $manager->persist($lieu);
+        }
+
+        $manager->flush();
+    }
+
+    public function addCampus(ObjectManager $manager): void
+    {
+        $campus = ['Saint-Herblain', 'Niort', 'La Roche-sur-Yon', 'Chartres-de-Bretagne'];
+
+        foreach ($campus as $campu) {
+            $campus = new Campus();
+            $campus->setNom($campu);
+            $manager->persist($campus);
+        }
+
+        $manager->flush();
+    }
+
+    public function addParticipants(int $number, ObjectManager $manager)
+    {
+
+        $roles = ['ROLE_USER', 'ROLE_ADMIN'];
+        $campus = $manager->getRepository(Campus::class)->findAll();
+        for ($i = 0; $i < $number; $i++) {
+            $participant = new Participant();
+            $participant->setPseudo($this->faker->userName);
+            $participant->setRoles([$this->faker->randomElement($roles)]);
+            $participant->setNom($this->faker->lastName);
+            $participant->setPrenom($this->faker->firstName);
+            $participant->setTelephone($this->faker->phoneNumber);
+            $participant->setMail($this->faker->email);
+            $participant->setCampus($this->faker->randomElement($campus));
+            $participant->setPassword($this->userPasswordHasher->hashPassword($participant, 'password'));
+            $manager->persist($participant);
+        }
+
+        $manager->flush();
+    }
+
+    public function addSorties(int $number, ObjectManager $manager)
+    {
+        $etats = $manager->getRepository(Etat::class)->findAll();
+        $lieux = $manager->getRepository(Lieu::class)->findAll();
+        $participants = $manager->getRepository(Participant::class)->findAll();
+        $campus = $manager->getRepository(Campus::class)->findAll();
+        for ($i = 0; $i < $number; $i++) {
+            $sortie = new Sortie();
+            $sortie->setNom($this->faker->sentence(3));
+            $sortie->setDateHeureDebut($this->faker->dateTimeBetween('now', '+1 year'));
+            $sortie->setDuree($this->faker->numberBetween(1, 10) * 60);
+            $sortie->setDateLimiteInscription($this->faker->dateTimeBetween('now', '+1 year'));
+            $sortie->setNbInscriptionsMax($this->faker->numberBetween(5, 20));
+            $sortie->setInfosSortie($this->faker->text);
+            $sortie->setEtat($this->faker->randomElement($etats));
+            $sortie->setLieu($this->faker->randomElement($lieux));
+            $sortie->setOrganisateur($this->faker->randomElement($participants));
+            $sortie->setCampus($this->faker->randomElement($campus));
+            $sortie->addParticipant($this->faker->randomElement($participants));
+            $manager->persist($sortie);
+        }
+
+        $manager->flush();
+    }
+
 }
