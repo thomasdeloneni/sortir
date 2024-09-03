a8a2110c3e93a07e60ced396bbdd4b8ffe845def
Antoine LEMARCHAND <antoine.lemarchand2023@campus-eni.fr>
2024-09-03 10:42:23 +0200

    formulaire de connexion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
