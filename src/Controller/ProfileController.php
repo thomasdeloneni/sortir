84fcb2cb9f30aaf1bb3f861bd7611bd2ccf64810
Antoine LEMARCHAND <antoine.lemarchand2023@campus-eni.fr>
2024-09-04 11:57:33 +0200

    gestion de la modification de mot de passe , affichage du nom des campus sur le profil, messages flash apres modification de profil

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -22,20 +23,30 @@ class ProfileController extends AbstractController
     }
 
     #[Route('/profile/update', name: 'app_profile_update')]
-    public function profileUpdate(Request $request, EntityManagerInterface $em, UserInterface $participant): Response
+    public function profileUpdate(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $participant = $this->getUser();
         $profilForm = $this->createForm(ProfilType::class, $participant);
         $profilForm->handleRequest($request);
 
         if($profilForm->isSubmitted() && $profilForm->isValid()){
+            $modifPassword = $profilForm->get('password')->getData();
+
+            if($modifPassword){
+                $hashedPassword = $passwordHasher->hashPassword($participant, $modifPassword);
+                $participant->setPassword($hashedPassword);
+            }
+
+            $em->persist($participant);
             $em->flush();
-            $this->addFlash('succès', 'on a valid" le formulaire !');
-            return $this->redirectToRoute('app_main');
+            $this->addFlash('success', 'Le profil a été mis à jour avec succès.');
+            return $this->redirectToRoute('app_profile');
         }
         return $this->render('profile/profile_update.html.twig', [
             'participant' => $participant,
             'profilForm' => $profilForm->createView(),
         ]);
     }
+
 }
 
--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -4,11 +4,12 @@ namespace App\Form;
 
 use App\Entity\Campus;
 use App\Entity\Participant;
-use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,12 +23,19 @@ class ProfilType extends AbstractType
             ->add('nom', TextType::class, ['label' => 'Nom'])
             ->add('telephone', TextType::class, ['label' => 'Téléphone'])    
             ->add('mail', EmailType::class, ['label' => 'mail'])
-            ->add('password')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'first_options'  => ['label' => 'Mot de passe', 'hash_property_path' => 'password'],
+                'second_options' => ['label' => 'Confirmation'],
+                'required' => false,
+                'mapped' => false,
+            ])
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nom',
             ])
-        ;
+        ; 
     }
 
     public function configureOptions(OptionsResolver $resolver): void
